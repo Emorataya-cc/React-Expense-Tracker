5cf86435572cc735c24ea9a1bc5aeac1ffd56f6b
Emorataya-cc@conestogac.on.ca <Emorataya-cc@conestogac.on.ca>
2021-06-25 20:08:47 -0400

    modifying App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
+
+  const expenses = [
+
+  ];
+
+  
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
